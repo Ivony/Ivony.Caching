c9b9478455a5cb3d468272ba29d977efd47993df
Ivony <ivony@live.com>
2015-11-21 17:28:21 +0800

    修正 Bug

--- Ivony.Caching/AsyncMultiCacheProvider.cs
+++ Ivony.Caching/AsyncMultiCacheProvider.cs
@@ -6,55 +6,81 @@ using System.Threading.Tasks;
 
 namespace Ivony.Caching
 {
+
+
+  /// <summary>
+  /// 实现异步多级缓存提供程序
+  /// </summary>
   public class AsyncMultiCacheProvider : IAsyncCacheProvider
   {
 
+    /// <summary>
+    /// 一级缓存提供程序
+    /// </summary>
+    public IAsyncCacheProvider L1Cache { get; }
 
-    private IAsyncCacheProvider L1;
-    private IAsyncL2CacheProvider L2;
 
 
-    public Task Clear()
+    /// <summary>
+    /// 二级缓存提供程序
+    /// </summary>
+    public IAsyncL2CacheProvider L2Cache { get; }
+
+
+
+    /// <summary>
+    /// 创建多级缓存提供程序对象
+    /// </summary>
+    /// <param name="l1Cache">一级缓存</param>
+    /// <param name="l2Cache">二级缓存</param>
+    public AsyncMultiCacheProvider( IAsyncCacheProvider l1Cache, IAsyncL2CacheProvider l2Cache )
     {
-      return Task.WhenAll( L1.Clear(), L2.Clear() );
+      L1Cache = l1Cache;
+      L2Cache = l2Cache;
     }
 
-    public async Task<object> Get( string key )
+
+    public virtual Task Clear()
     {
-      var value = await L1.Get( key );
+      return Task.WhenAll( L1Cache.Clear(), L2Cache.Clear() );
+    }
+
+    public virtual async Task<object> Get( string key )
+    {
+      var value = await L1Cache.Get( key );
 
       if ( value != null )
         return value;
 
-      var cacheItem = await L2.GetCacheItem( key );
+      var cacheItem = await L2Cache.GetCacheItem( key );
       if ( cacheItem == null )
         return null;
 
 
       //后台写入一级缓存
-      Background( L1.Set( cacheItem ) );
+      Background( L1Cache.Set( cacheItem ) );
       return cacheItem.Value;
     }
 
-    public Task Remove( string cacheKey )
+    public virtual Task Remove( string cacheKey )
     {
 
-      return Task.WhenAll( L1.Remove( cacheKey ), L2.Remove( cacheKey ) );
+      return Task.WhenAll( L1Cache.Remove( cacheKey ), L2Cache.Remove( cacheKey ) );
 
     }
 
-    public async Task Set( string key, object value, CachePolicy cachePolicy )
+    public virtual async Task Set( string key, object value, CachePolicy cachePolicy )
     {
 
       var cacheItem = new CacheItem( key, value, cachePolicy );
 
       //清除一级缓存，写入二级缓存
 
-      await L1.Remove( key );
-      await L2.Set( cacheItem );
+      await L1Cache.Remove( key );
+      await L2Cache.Set( cacheItem );
 
       //后台写入一级缓存。
-      Background( L1.Set( cacheItem ) );
+      Background( L1Cache.Set( cacheItem ) );
 
     }
 
