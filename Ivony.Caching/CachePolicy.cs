580f30561eaf4f0d78828919118b15768eb5b54b
Ivony <ivony@live.com>
2016-01-15 17:06:33 +0800

    优化代码

---
+++ Ivony.Caching/CachePolicy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ivony.Caching
+{
+
+  /// <summary>
+  /// 定义缓存策略
+  /// </summary>
+  public class CachePolicy
+  {
+
+    /// <summary>
+    /// 创建缓存策略
+    /// </summary>
+    /// <param name="expires">过期时间</param>
+    public CachePolicy( DateTime expires )
+    {
+
+      if ( expires.Kind != DateTimeKind.Utc )
+        throw new ArgumentException( "expires must be an UTC time.", "expires" );
+
+
+      Expires = expires;
+
+    }
+
+
+    /// <summary>
+    /// 缓存失效时间
+    /// </summary>
+    public DateTime Expires { get; }
+
+  }
+}
